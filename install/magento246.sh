7bf054066a73c02e6eafd3d79a94b9b3f662b38a
Rinto George <mail.rinto@gmail.com>
2023-08-08 18:02:18 +0400

    Added magento installer script

--- install/magento246.sh
+++ install/magento246.sh
@@ -1,1 +1,21 @@
-Test
+php bin/magento setup:install \
+--base-url=http:/127.0.0.1 \
+--backend-frontname=admin_cp \
+--db-host=localhost \
+--db-name=test_magento \
+--db-user=root \
+--db-password=GFM1h%qJ!zkEarW1m}^5 \
+--admin-firstname=admin \
+--admin-lastname=admin \
+--admin-email=mail.rinto@gmail.com \
+--admin-user=admin \
+--admin-password=admin123 \
+--language=en_US \
+--currency=AED \
+--timezone=America/Chicago \
+--use-rewrites=1 \
+--search-engine=elasticsearch7 \
+--elasticsearch-host=localhost \
+--elasticsearch-port=9200 \
+--elasticsearch-index-prefix=magento2 \
+--elasticsearch-timeout=15
